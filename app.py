7b36053ccf75bcc705cf1a8db525a97a92f548f3
Sai Sharma Parnandi <106370471+SaiSharma-007@users.noreply.github.com>
2022-12-30 15:06:06 +0530

    Updated Work
    Our project is titled as **Cryptogram**. Some unnecessary links in Nav bars are removed.
    Logo added to the nav bar.
    Index / Home page is added to project with hidden content about project.
    Transaction page is added.
    **All pages are linked to each other**
    For any queries contact me or @Jesh-17 .
    
    Co-Authored-By: Jesh-17 <83612190+Jesh-17@users.noreply.github.com>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('Admin.html')
+    return render_template('Index.html')
 
 @app.route('/Admin')
 def Admin():
@@ -16,6 +16,10 @@ def Registrationpythom ():
 @app.route('/Login')
 def Login():
     return render_template('Login.html')
+
+@app.route('/Transaction')
+def Transaction():
+    return render_template('Transaction.html')
 
 '''@app.route('/Reg_submit',methods=['POST','GET'])
 def Reg_submit():
--- static/Particles/validate.js
+++ static/Particles/validate.js
@@ -17,3 +17,17 @@
     }, false)
   })
 })()
+
+const doc = document.getElementsByClassName('collapsible')
+
+      for (let i = 0; i < doc.length; i++) {
+        doc[i].addEventListener('click', function () {
+          this.classList.toggle('active')
+          let content = this.nextElementSibling
+          if (content.style.display === 'block') {
+            content.style.display = 'none'
+          } else {
+            content.style.display = 'block'
+          }
+        })
+      }
