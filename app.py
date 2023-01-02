5997987252bc31c4bcd067847e647fb3192b374d
madhuripani <79073618+madhuri970@users.noreply.github.com>
2023-01-02 12:08:04 +0530

    backend

--- app.py
+++ app.py
@@ -1,5 +1,58 @@
 from flask import Flask,redirect,url_for,render_template,request
+#import libraries
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
 app=Flask(__name__)
+
+#code for connection
+app.config['MYSQL_HOST'] = 'localhost'#hostname
+app.config['MYSQL_USER'] = 'root'#username
+app.config['MYSQL_PASSWORD'] = ''#password
+#in my case password is null so i am keeping empty
+app.config['MYSQL_DB'] = 'blockchain'#database name
+
+mysql = MySQL(app)
+
+@app.route('/')
+
+@app.route('/Registration',methods=['GET','POST'])
+def projectreg():
+    msg=''
+    #applying empty validation
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form and 'uname' in request.form and 'firstName' in request.form and 'lastName' in request.form and 'city' in request.form  and 'state' in request.form and 'zip' in request.form and 'aadhar' in request.form and 'pkey' in request.form :
+        #passing HTML form data into python variable
+        
+        email= request.form['email']
+        password = request.form['password']
+        uname = request.form['uname']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        city = request.form['city']
+        state = request.form['state']
+        zip = request.form['zip']
+        aadhar = request.form['aadhar']
+        pkey = request.form['pkey']
+        #creating variable for connection
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        #query to check given data is present in database or no
+        cursor.execute('SELECT * FROM details WHERE aadhar = % s', (aadhar,))
+        #fetching data from MySQL
+        result = cursor.fetchone()
+        if result:
+            msg = 'Account already exists !'
+        else:
+            #executing query to insert new data into MySQL
+            cursor.execute('INSERT INTO details (firstName, lastName, email, uname, aadhar, password, city, state,zip,pkey) VALUES (% s,% s,% s,% s,% s,% s,% s,% s,% s,% s)', (firstName, lastName, email, uname, aadhar, password, city, state,zip,pkey))
+            mysql.connection.commit()
+            #displaying message
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('Registration.html', msg=msg)
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
 
 @app.route('/')
 def home():
